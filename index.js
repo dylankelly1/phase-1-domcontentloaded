23c1b0ff25c047de2d9d51adb37215c84f04ef3a
Dylan Kelly <init.defaultBranch>
2023-06-06 19:41:37 -0500

    commit tree f0b9e467a0875e9e793c0f488ca90f22f779a6ef

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+  function changeText() {
+    const paragraph = document.querySelector('p');
+    paragraph.textContent = "This is really cool!";
+  }
+
+  document.addEventListener('DOMContentLoaded', changeText);
